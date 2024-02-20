5d8ca6219d0ef789de4ace49a3074e7e732a932e
hamno996 <hamnomhsen@gmail.com>
2024-02-20 15:37:22 +0300

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className=" h-full items-center justify-center flex">{children}</div>
+	);
+};
+
+export default AuthLayout;
---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,27 @@
+import { auth as clerkAuth } from "@clerk/nextjs";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+  const { userId } = clerkAuth();
+
+  if (!userId) throw new Error("Unauthorized");
+  return { userId };
+};
+
+const auth = (req: Request) => ({ id: "fakeId" });
+
+export const ourFileRouter = {
+  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  courseAttachment: f(["text", "image", "video", "audio", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
