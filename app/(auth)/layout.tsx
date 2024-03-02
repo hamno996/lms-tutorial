8428b45de8e442405bec33b4d19f44ea7dcb4f38
hamno996 <hamnomhsen@gmail.com>
2024-03-02 20:09:57 +0300

    abc

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className=" h-full items-center justify-center flex">{children}</div>
+		<div className="h-full items-center justify-center flex">{children}</div>
 	);
 };
 
